2777e6b92c5133db4d28e9f483712215fecd02c0
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-04 11:45:10 +0000

    Baby Weather App

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,10 +1,22 @@
-console.log("Mwaka");
-fruit = prompt("What is your favorite fruit?: ")
-console.log(fruit)
+//JavaScript 101
 
-food = Number(prompt("How much was the food?: "))
-tipPercentage = Number(prompt("tip percentage"))/ 100 
-tipAmount = food * tipPercentage
-total = food + tipAmount
-console.log("tipAmount    : ",tipAmount)
-console.log("total Amount : ",total)
+// console.log("Mwaka");
+// fruit = prompt("What is your favorite fruit?: ")
+// console.log(fruit)
+
+// food = Number(prompt("How much was the food?: "))
+// tipPercentage = Number(prompt("tip percentage"))/ 100 
+// tipAmount = food * tipPercentage
+// total = food + tipAmount
+// console.log("tipAmount    : ",tipAmount)
+// console.log("total Amount : ",total)
+
+////////////////////////////////////////////////
+//  Baby Weather App ////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////
+let weather = prompt("How is the weather?")
+if (weather == "rainy") {
+  console.log("Get an umbrella")
+}else {
+  console.log("Wear your sunglasses")
+}
